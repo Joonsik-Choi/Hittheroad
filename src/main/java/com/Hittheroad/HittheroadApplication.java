7dddde86f11e9406c13e15aaad8511b925a16b9d
wnstlr0615 <wnstlr0615@naver.com>
2020-12-01 16:02:06 +0900

    Initial commit

---
+++ src/main/java/com/Hittheroad/HittheroadApplication.java
@@ -0,0 +1,13 @@
+package com.Hittheroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HittheroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HittheroadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Hittheroad/HittheroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hittheroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HittheroadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
