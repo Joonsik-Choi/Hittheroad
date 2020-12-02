1b170d536e6f9ef0b3d498d78a0da1d855e5d6cb
wnstlr0615 <wnstlr0615@naver.com>
2020-12-02 14:17:02 +0900

    웹 기초 셋팅

--- src/main/java/com/Hittheroad/HittheroadApplication.java
+++ src/main/java/com/Hittheroad/HittheroadApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HittheroadApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HittheroadApplication.class, args);
 	}
-
 }
--- src/main/java/com/Hittheroad/Interfaces/IndexController.java
+++ src/main/java/com/Hittheroad/Interfaces/IndexController.java
@@ -1,12 +1,21 @@
 package com.Hittheroad.Interfaces;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
     @GetMapping("/")
     public String index(){
-        return "Welcome";
-    }
+        return "hittheroad/index";
+    }//홈
+
+    @GetMapping("/login")
+    public String login(){return "hittheroad/login";}//로그인 화면
+
+    @GetMapping("sign_in")
+    public String signIn(){return "hittheroad/sign_in";} //회원 가입
+
+    @GetMapping("sign_up")
+    public String signUp(){return "hittheroad/sign_up";}//로그인 시도
 }
